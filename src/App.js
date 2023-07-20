1682b6b6b7c8f2b3ba94f8a573436b2fc7bdde0f
Abdulbosid <akamalov207@gmail.com>
2023-07-20 11:41:36 +0600

    1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './screens/main';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/screens/main.jsx
+++ src/screens/main.jsx
@@ -1,10 +1,50 @@
+import axios from 'axios';
+import { useState } from 'react';
 const Main = () => {
+    const [api, setapi] = useState([]);
+    const [search, setSearch] = useState(false);
+    const [value, setValue] = useState(null);
+    const fetchMovies = async () => {
+        const data = await axios({
+            method: "get",
+            url: `https://api.genderize.io/?name=${value}`
+        })
+        console.log('movies', data);
+        if (data.status == 200) {
+            setSearch(true);
+            setapi(data.data);
+        } else {
+            setSearch(false);
+        }
+    }
     return (
         <>
-            <div class="input-group mb-3">
-                <button class="btn btn-outline-secondary" type="button" id="button-addon1">Button</button>
-                <input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1"/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-4">
+                        <div className="input-group mb-3">
+                            <button className="btn btn-outline-secondary" type="button" id="button-addon1" onClick={fetchMovies}>Button</button>
+                            <input type="text" className="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1" onChange={(e) => { setValue(e.target.value) }} required />
+                            {search ?
+                                <>
+                                    
+                                            <>
+                                                <div className="bg-dark text-white col-12 p-2">
+                                                    {api.gender}
+                                                </div>
+                                            </>
+
+                                </>
+                                :
+                                <>
+
+                                </>
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </>
     )
 }
